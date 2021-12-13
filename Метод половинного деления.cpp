05507f769972969748659b32d2c91611f5a1b5b0
Галавтдинов Станислав Сергеевич ИВТ-21-1Б <91310045+gals59911@users.noreply.github.com>
2021-12-13 22:13:44 +0500

    Add files via upload

---
+++ Метод половинного деления.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    float a, b, c;
+    float fA, fB, fC;
+    double eps = 0.000001;
+    a = 1;
+    b = 2;
+    while (b - a > eps)
+    {
+        c = (a + b) / 2;
+        fA = (pow(a, 3) - a - 1);
+        fB = (pow(b, 3) - b - 1);
+        fC = (pow(c, 3) - c - 1);
+        if (fA * fC < 0) { b = c; }
+        else if (fC * fB < 0) { a = c; }
+        else { cout << "Что-то не то" << endl; }
+    }
+    cout << a << b << endl;
+}
