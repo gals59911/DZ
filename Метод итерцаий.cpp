005b351b3b16b0eb530f23eaad4b3d0756da12fa
Галавтдинов Станислав Сергеевич ИВТ-21-1Б <91310045+gals59911@users.noreply.github.com>
2021-12-13 22:18:58 +0500

    Add files via upload

---
+++ Метод итерцаий.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    float x, xP;
+    x = 2;
+    xP = 0;
+    double eps = 0.000001;
+    while (abs(x - xP) > eps)
+    {
+        xP = x;
+        x = (-0.05) * (pow(xP, 3) - xP - 1) + xP;
+    }
+    cout << x << xP << endl;
+}
