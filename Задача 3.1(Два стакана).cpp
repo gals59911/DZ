ec0489e61312ca3b5ec0d498e055ad9bc4107901
Галавтдинов Станислав Сергеевич ИВТ-21-1Б <91310045+gals59911@users.noreply.github.com>
2021-12-13 22:39:22 +0500

    Add files via upload

---
+++ Задача 3.1(Два стакана).cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5], n;
+    n = 5;
+    int key;
+    // Напиши ввод сам!!!!!!!!!!!!!!!!
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int j = 1; j < n; j++)
+    {
+        key = arr[j];
+        int i = j - 1;
+        while (i >= 0 && arr[i] > key)
+        {
+            arr[i + 1] = arr[i];
+            i--;
+        }
+        arr[i + 1] = key;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
