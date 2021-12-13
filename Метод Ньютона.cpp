11160142c3ad50d6a3fbb9bd33997bf724198e84
Галавтдинов Станислав Сергеевич ИВТ-21-1Б <91310045+gals59911@users.noreply.github.com>
2021-12-13 22:15:37 +0500

    Add files via upload

---
+++ Метод Ньютона.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    float x, xP;
+    x = 2;
+    xP = 0;
+    double eps = 0.000001;
+    while (abs(x - xP) > eps)
+    {
+        xP = x;
+        x = xP - (pow(xP, 3) - xP - 1) / (3 * pow(xP, 2) - 1);
+    }
+    cout << x << xP << endl;
+}
