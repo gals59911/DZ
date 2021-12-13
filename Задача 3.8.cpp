dcdf2455f1f1471ec87f28490077407449b3d6fe
Галавтдинов Станислав Сергеевич ИВТ-21-1Б <91310045+gals59911@users.noreply.github.com>
2021-12-13 22:36:01 +0500

    Add files via upload

---
+++ Задача 3.8.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n, tmp;
+    n = 7;
+    int arr[7] = { 41,223,23,12,23,34,56 };
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < n / 2; i++)
+    {
+        // swap - оперция обмена
+        tmp = arr[i];
+        arr[i] = arr[n - i - 1];
+        arr[n - i - 1] = tmp;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
