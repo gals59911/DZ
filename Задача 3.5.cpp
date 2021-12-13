7c242d58baf8ec801ba2a863618a8eba007e8d76
Галавтдинов Станислав Сергеевич ИВТ-21-1Б <91310045+gals59911@users.noreply.github.com>
2021-12-13 22:25:37 +0500

    Add files via upload

---
+++ Задача 3.5.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int ch, max, count;
+	count = 0;
+	max = 0;
+	int a[10];
+	for (int i = 0; i < 10; i++)
+	{
+		cin >> ch;
+		a[i] = ch;
+		if (a[i] > max)
+		{
+			max = a[i];
+		}
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		if (max == a[i])
+		{
+			count++;
+		}
+	}
+	cout <<"Max element:"<< max << " Kol:" << count << endl;
+}
