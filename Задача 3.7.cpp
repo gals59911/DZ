2c09b40a1b56ffa710bcbcec25f6e2242fe31d69
Галавтдинов Станислав Сергеевич ИВТ-21-1Б <91310045+gals59911@users.noreply.github.com>
2021-12-13 22:34:58 +0500

    Add files via upload

---
+++ Задача 3.7.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 5,ch;
+    int a[5];
+    bool flag = true;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> ch;
+        a[i] = ch;
+    }
+    for (int i = n - 1; i > 0; i--)
+    {
+        if (a[i] < a[i - 1]) { flag = false; }
+    }
+    if (flag)
+    {
+        cout << "Aray is ordered" << endl;
+    }
+    else
+    {
+        cout << "Aray is not ordered" << endl;
+    }
+}
