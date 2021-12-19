4bef83b1a43ecaecf0fe2138a1f420c10f7dfc24
Галавтдинов Станислав Сергеевич ИВТ-21-1Б <91310045+gals59911@users.noreply.github.com>
2021-12-20 00:51:48 +0500

    Add files via upload

---
+++ Задача 3.6.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int ch, max,min;
+	max = 0;
+	min = 0;
+	int a[10];
+	for (int i = 0; i < 10; i++)
+	{
+		cin >> ch;
+		min = ch;
+		a[i] = ch;
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		if (a[i] > max)
+		{
+			max = a[i];
+		}
+		if (a[i] < min)
+		{
+			min = a[i];
+		}
+	}
+	cout << "Max element:" << max << endl;
+	cout << "Min element:" << min << endl;
+}
