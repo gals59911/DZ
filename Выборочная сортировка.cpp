4b0e985df386273fa839a75c0c3883556b6dec4b
Галавтдинов Станислав Сергеевич ИВТ-21-1Б <91310045+gals59911@users.noreply.github.com>
2021-12-20 12:49:33 +0500

    Add files via upload

---
+++ Выборочная сортировка.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int n = 6;
+    int min_index, tmp;
+    int arr[6] = { 5, 2, 4, 6, 1, 3 };
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        min_index = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_index])
+            {
+                min_index = j;
+            }
+        }
+        tmp = arr[min_index];
+        arr[min_index] = arr[i];
+        arr[i] = tmp;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+}
