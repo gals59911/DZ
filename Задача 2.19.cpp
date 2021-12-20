648627405771b94dc264595aedb1166b468a3679
Галавтдинов Станислав Сергеевич ИВТ-21-1Б <91310045+gals59911@users.noreply.github.com>
2021-12-21 00:46:09 +0500

    Add files via upload

---
+++ Задача 2.19.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	int n;
+	int sum = 0;
+	int j = 1;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		if (i == pow(3, j))
+		{
+			sum -= i;
+			j += 1;
+		}
+		else
+		{
+			sum += i;
+		}
+
+	}
+	cout << sum;
+	return 0;
+}
