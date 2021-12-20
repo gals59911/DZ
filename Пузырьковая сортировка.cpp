ef4739c1d9aa9bb5abe8fa9481e2ef262d3bc31d
Галавтдинов Станислав Сергеевич ИВТ-21-1Б <91310045+gals59911@users.noreply.github.com>
2021-12-20 13:44:50 +0500

    Add files via upload

---
+++ Пузырьковая сортировка.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5], n,tmp;
+    n = 5;
+    int key;
+    for (int i = 0; i < n; i++)
+    {
+        cin >>arr[i];
+    }
+    for (int j = 0; j < n - 1; j++)
+    {
+        if (arr[j] > arr[j + 1])
+        {
+            tmp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = tmp;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+}
